5137ec17af63c0a3c11221a915c0518ecfe5284b
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-10-31 09:31:30 +0100

    lezione oggetti

---
+++ corso/settimana_2_javascript/l6_31_10_2023/lezioni/oggetti.js
@@ -0,0 +1,17 @@
+var oggettoVuoto = {};
+var persona = {
+    nome:'Federico',
+    cognome:'DeAmbrosis',
+    eta:42
+};
+var persona2 = {
+    nome:'Federico',
+    cognome:'DeAmbrosis',
+    eta:42,
+    indirizzo: {
+        via:'pippo',
+        numero: 1,
+        cap: '00100',
+        citta: 'Milano'
+    }
+};
