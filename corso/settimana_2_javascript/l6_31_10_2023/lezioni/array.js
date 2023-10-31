536df2433f476bc55b724d78f1285f4bef8f9885
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-10-31 10:08:34 +0100

    array

---
+++ corso/settimana_2_javascript/l6_31_10_2023/lezioni/array.js
@@ -0,0 +1,16 @@
+var giorniDellaSettimana = [
+    'lunedi',
+    'martedi',
+    'mercoledi',
+    'giovedi',
+    'venerdi',
+    'sabato',
+    'domenica'
+];
+var primoGiorno = giorniDellaSettimana[0];
+var terzoGiorno = giorniDellaSettimana[2];
+var settimoGiorno = giorniDellaSettimana[6];
+
+console.log(primoGiorno);
+console.log(terzoGiorno);
+console.log(settimoGiorno);
