33c87c8e5de74e3de45570313044b638d0e0138b
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-10-31 09:55:12 +0100

    oggetti

--- corso/settimana_2_javascript/l6_31_10_2023/lezioni/oggetti.js
+++ corso/settimana_2_javascript/l6_31_10_2023/lezioni/oggetti.js
@@ -15,3 +15,26 @@ var persona2 = {
         citta: 'Milano'
     }
 };
+
+// accedere a proprietà oggetto : dot notation
+var nome = persona.nome;
+//oppure ma , usato solo se i nomi degli atributi hanno caratteri speciali.
+var nome = persona["nome"];
+//mostrare citta in console
+console.log(persona2.indirizzo.citta);
+//definisco una nuova proprietà dell'oggetto. gli oggetti sono dinamici.
+persona2.residenza = "Milano";
+console.log(persona2);
+//oppure prima dichiaro un oggetto e poi lo inizializzo
+var persona3 ={};
+persona3.nome= 'lui';
+persona3.cognome='egli';
+persona3.eta='50';
+persona3.indirizzo = {
+    via:'pippo',
+    numero:1
+};
+// cancellare un attributo
+
+delete persona3.eta;
+console.log(persona3);
