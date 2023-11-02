bfde2fb9f5bcf020752c1e485127317fa6217ea2
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-11-02 11:59:14 +0100

    for annidato

--- corso/settimana_2_javascript/l7_02_11_2023/lezioni/cicli.js
+++ corso/settimana_2_javascript/l7_02_11_2023/lezioni/cicli.js
@@ -22,12 +22,26 @@ while (n <= 10) {
 document.getElementById('demo').innerHTML=text;
 */
 
-var utenti1 = ['mario','peppe','aldo','giovanni','giacomo'];
-var utenti2 = ['mario2','peppe2','aldo2','giovanni2','giacomo2'];
+var utenti1 = ['mario', 'peppe', 'aldo', 'giovanni', 'giacomo'];
+var utenti2 = ['mario2', 'peppe2', 'aldo2', 'giovanni2', 'giacomo2'];
 
-for (i=0; i<utenti1.length;i++) {
+for (i = 0; i < utenti1.length; i++) {
     console.log(utenti1[i]);
 }
 for (elemento in utenti2) {
     console.log(utenti2[elemento]);
+}
+
+let studentsData = [['Alessandro', 24], ['Antonio', 22], ['alice', 32]]
+
+for (let i = 0; i < studentsData.length; i++) {
+
+    let innerArrayLength = studentsData[i].length;
+
+    for (let studente = 0; studente < innerArrayLength; studente++) {
+
+        console.log(studentsData[i][studente]);
+
+    }
+
 }
