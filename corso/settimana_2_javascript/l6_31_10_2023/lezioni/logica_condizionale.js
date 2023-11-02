6507f1b0f27a778cecb6dd6db2cdbd538681d668
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-11-02 08:51:59 +0100

    format

--- corso/settimana_2_javascript/l6_31_10_2023/lezioni/logica_condizionale.js
+++ corso/settimana_2_javascript/l6_31_10_2023/lezioni/logica_condizionale.js
@@ -7,26 +7,26 @@ var num1 = 12;
 var num2 = 10;
 var x = 7;
 
-if (num1>num2){
-    console.log(num1+' è > di '+num2);
-    document.getElementById('test1').innerHTML= num1+' è > di '+num2;
+if (num1 > num2) {
+    console.log(num1 + ' è > di ' + num2);
+    document.getElementById('test1').innerHTML = num1 + ' è > di ' + num2;
 }
-else if(num1==num2) {
-    console.log(num1+' è = a '+num2);
-    document.getElementById('test1').innerHTML=num1+' è = a '+num2;
+else if (num1 == num2) {
+    console.log(num1 + ' è = a ' + num2);
+    document.getElementById('test1').innerHTML = num1 + ' è = a ' + num2;
 }
 else {
-    console.log(num1+' è < di '+num2);
-    document.getElementById('test1').innerHTML=num1+' è < di '+num2;
+    console.log(num1 + ' è < di ' + num2);
+    document.getElementById('test1').innerHTML = num1 + ' è < di ' + num2;
 }
 
-if (x%2==0){
-    console.log(x+' è pari');
-    document.getElementById('test2').innerHTML=x+' è pari';
+if (x % 2 == 0) {
+    console.log(x + ' è pari');
+    document.getElementById('test2').innerHTML = x + ' è pari';
 }
-else{
-    console.log(x+' è dispari');
-    document.getElementById('test2').innerHTML=x+' è dispari';
+else {
+    console.log(x + ' è dispari');
+    document.getElementById('test2').innerHTML = x + ' è dispari';
 }
 
 // accenno ai costruttori
