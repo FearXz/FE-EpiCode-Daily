70b608fced0672466b7b71074af7d04083fb5b32
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-11-02 09:29:27 +0100

    switch

---
+++ corso/settimana_2_javascript/l7_02_11_2023/lezioni/cicli.js
@@ -0,0 +1,12 @@
+let x = 4;
+
+switch (x % 2) {
+    case 0: messaggio = x + ' è pari';
+        break;
+
+    case 1: messaggio = x + ' è dispari';
+        break;
+
+    default: messaggio = x + ' non è un numero';
+        break;
+}
