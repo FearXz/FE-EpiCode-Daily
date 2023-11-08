77137a28847bd5d5a3fea438edb17ddeacb73a47
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-11-08 18:01:20 +0100

    commit

--- corso/settimana_3_javascript2/l11_08_11_2023/esercizi/asset/js/index.js
+++ corso/settimana_3_javascript2/l11_08_11_2023/esercizi/asset/js/index.js
@@ -9,29 +9,29 @@ const handleSubmit = function (e) {
 const addTask = function () {
 
     let inputField = document.getElementById('inputText').value;
-
     let containerDiv = document.getElementById('containerDiv');
+
     let newDiv = document.createElement('div');
     let newP = document.createElement('p');
     let newB = document.createElement('button');
-    newP.textContent = inputField;
+    newP.innerHTML = inputField;
     newB.innerHTML = 'Delete';
     newB.classList.add('remove');
     newDiv.classList.add('task');
     newDiv.appendChild(newP);
     newDiv.appendChild(newB);
     containerDiv.appendChild(newDiv);
+   // document.getElementById('inputText').value = '';
 
 }
 
 const attachComplete = function () {
 
-    const allP = document.querySelectorAll('p');
+    let all = document.querySelectorAll('.task');
 
-    for (let i = 0; i < allP.length; i++) {
-        allP[i].addEventListener('click', function () { this.classList.toggle('line') })
+    for (let i = 0; i < all.length; i++) {
+        all[i].addEventListener('click', function () { this.classList.toggle('line') })
     }
-
 }
 
 const attachDelete = function () {
@@ -40,7 +40,7 @@ const attachDelete = function () {
     const remove = document.querySelectorAll('.remove')
 
     for (let i = 0; i < task.length; i++) {
-        remove[i].addEventListener('click', function () { task[i].remove() })
+        remove[i].addEventListener('click', function () { this.parentNode.remove() })
     }
 
 }
