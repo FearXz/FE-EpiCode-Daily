e4b021de84da00e4071916d01ef0eb90a4c10b5a
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-12-05 16:10:42 +0100

    start

---
+++ corso/settimana_6_js_avanzato/l21_05_12_2023/esercizi/assets/js/index.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  let form = document.getElementsByTagName("form");
+form.addEventListener('submit',handleSubmit);
+
+class 
+
+};
+let handleSubmit = () => {
+
+}
+
+let registerPet = () => {
+
+}
